ca1f16cbc0ea46296c92650854a88fb308aae9fd
jorgeromero17 <jorgeromero98@uotlook.com>
2021-11-13 23:10:57 -0600

    frontend home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,46 @@
 
 @section('content')
 
-       <h1>Este sera el home</h1> 
+<div class="div-container">
+   <div class="movies-container">
+      <div class="movie">
+         <a href="#" class="movie-photo shadow" style="background-image: url(https://phantom-marca.unidadeditorial.es/e522d2e2fd0772e0462ca8d15e3a05ee/resize/640/assets/multimedia/imagenes/2019/09/17/15687205359826.jpg)"></a>
+         <p>Titulo largo de pelicula para probar</p>
+      </div>
+      <div class="movie">
+         <a href="#" class="movie-photo shadow" style="background-image: url(https://academiaplay.es/wp-content/uploads/2016/05/Se7en-–-David-Fincher.jpg)"></a>
+         <p class="">Titulo corto</p>
+      </div>
+      <div class="movie">
+         <a href="#" class="movie-photo shadow" style="background-image: url(https://phantom-marca.unidadeditorial.es/e522d2e2fd0772e0462ca8d15e3a05ee/resize/640/assets/multimedia/imagenes/2019/09/17/15687205359826.jpg)"></a>
+         <p>Titulo largo de pelicula para probar</p>
+      </div>
+      <div class="movie">
+         <a href="#" class="movie-photo shadow" style="background-image: url(https://academiaplay.es/wp-content/uploads/2016/05/Se7en-–-David-Fincher.jpg)"></a>
+         <p>Titulo corto</p>
+      </div>
+      <div class="movie">
+         <a href="#" class="movie-photo shadow" style="background-image: url(https://academiaplay.es/wp-content/uploads/2016/05/Se7en-–-David-Fincher.jpg)"></a>
+         <p>Titulo corto</p>
+      </div>
+      <div class="movie">
+         <a href="#" class="movie-photo shadow" style="background-image: url(https://academiaplay.es/wp-content/uploads/2016/05/Se7en-–-David-Fincher.jpg)"></a>
+         <p>Titulo corto</p>
+      </div>
+      <div class="movie">
+         <a href="#" class="movie-photo shadow" style="background-image: url(https://academiaplay.es/wp-content/uploads/2016/05/Se7en-–-David-Fincher.jpg)"></a>
+         <p>Titulo corto</p>
+      </div>
+   </div>
+</div>
+{{-- <div class="div-main">
+   <h1 class="h4 p-3">MOVIES IN STOCK</h1>
+   <div class="div-movies">
+      <div class="movie">
+         <div class="movie-photo" style="background-image: url(https://phantom-marca.unidadeditorial.es/e522d2e2fd0772e0462ca8d15e3a05ee/resize/640/assets/multimedia/imagenes/2019/09/17/15687205359826.jpg)"></div>
+         <p>Titulo largo de pelicula para probar</p>
+      </div>
+   </div>
+</div> --}}
 
 @endsection
