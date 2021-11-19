37087f6973fda7894ef4e501e0b1185a134dd04b
Cristian <crissge2016@gmail.com>
2021-11-19 15:28:41 -0600

    Base de datos recional de compra y alquiler

---
+++ database/migrations/2021_11_19_201925_create_rentals_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRentalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rentals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('days_rented');
+            $table->integer('days_late')->nullable();
+            $table->integer('penalty_fee')->nullable();
+            $table->timestamp('delivery_date')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('movie_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('movie_id')->references('id')->on('movies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rentals');
+    }
+}
