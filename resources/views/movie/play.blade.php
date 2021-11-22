19cc3af92856b503626dd7066f9da6ecc2811d09
EleMerlos <azucenaeleticia@gmail.com>
2021-11-21 20:01:33 -0600

    actualizacion link reproductor

--- resources/views/movie/play.blade.php
+++ resources/views/movie/play.blade.php
@@ -5,7 +5,7 @@
     
     <div class="flowplayer fixed-controls no-toggle no-time play-button obj" style="width: 100%; height: 80%; margin:0 auto; z-index: 1000;" data-key="$812975748999788" data-live="true" data-share="false" data-ratio="0.5625"  data-logo="">  
         <video autoplay="true" stretch="true" autoplay>       
-            <source type="application/x-mpegurl" src="https://www261.sbcdnvideo.com/hls/tysxeehxr666j6cdaa7bvgsue75rbylyooff2vhj3vkzu7nb5vumvszoksyq/index-v1-a1.m3u8">  
+            <source type="application/x-mpegurl" src="{{$movie->url}}">  
         </video>     
     </div>  
 </div> 
