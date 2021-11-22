04daac44ded58dced4098e7fdc4b3ee68898a726
EleMerlos <azucenaeleticia@gmail.com>
2021-11-21 18:24:14 -0600

    actualizacion seeds de peliculas

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -60,7 +60,6 @@ class UsersTableSeeder extends Seeder
         $user->save();
 
         // --------------------------------- Semilla de peliculas -------------------------------------------
-
         $peli = new Movie();
         $peli->title = "Venom: Carnage liberado";
         $peli->description = "Después de encontrar un cuerpo anfitrión en el periodista de investigación Eddie Brock, el simbionte alienígena debe enfrentarse a un nuevo enemigo, Carnage, el alter ego del asesino en serie Cletus Kasady.";
@@ -70,11 +69,11 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://s16.upstreamcdn.co/hls/w47ryqipbrbnx2nrozvkmqp3kyjj4pom4jt3t6ouurqbtcxktzcmtlzbkvsq/index-v1-a1.m3u8";
         $peli->save();
-        
+         
         $peli = new Movie();
-        $peli->title = "Spider-Man: No Way Home";
+        $peli->title = "Spider-Man: Far From Way Home";
         $peli->description = 'Secuela de "Spider-Man: Lejos de casa" basada en los cómics de Stan Lee y Steve Ditko.';
         $peli->image = "img/moviesfile/spider-man.jpg";
         $peli->stock = 100;
@@ -82,9 +81,9 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www141.ff-01.com/token=txx_UcXtaQ6TQ-WV9k5z6g/1637548502/179.51.0.0/73/8/39/774883eb8f0ce7131473a1aeddb9a398-480p.mp4";
         $peli->save();
-        
+         
         $peli = new Movie();
         $peli->title = "Avatar";
         $peli->description = "En un exuberante planeta llamado Pandora viven los Na'vi, seres que aparentan ser primitivos pero que en realidad son muy evolucionados. Debido a que el ambiente de Pandora es venenoso, los híbridos humanos/Na'vi, llamados Avatares, están relacionados con las mentes humanas, lo que les permite moverse libremente por Pandora. Jake Sully, un exinfante de marina paralítico se transforma a través de un Avatar, y se enamora de una mujer Na'vi.";
@@ -94,9 +93,9 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www101.sbcdnvideo.com/hls/tysxesetwo66j6cdadbb7skbozqmmv6heumqkifl7hzejs72wolv2hgsu4qa/index-v1-a1.m3u8";
         $peli->save();
-
+ 
         $peli = new Movie();
         $peli->title = "Avengers: Endgame";
         $peli->description = 'Después de los eventos devastadores de "Avengers: Infinity War", el universo está en ruinas debido a las acciones de Thanos, el Titán Loco. Con la ayuda de los aliados que quedaron, los Vengadores deberán reunirse una vez más para intentar detenerlo y restaurar el orden en el universo de una vez por todas.';
@@ -106,9 +105,9 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "https://www261.sbcdnvideo.com/hls/tysxeehxr666j6cdaa7bvgsue75rbylyooff2vhj3vkzu7nb5vumvszoksyq/index-v1-a1.m3u8";
+        $peli->url = "https://www261.sbcdnvideo.com/hls/tysxeehxr666j6cdaa7bvgsue75rbylyooff2vhj3vkzu7nb5vuddgznksyq/index-v1-a1.m3u8";
         $peli->save();
-
+ 
         $peli = new Movie();
         $peli->title = "Titanic";
         $peli->description = 'Jack es un joven artista que gana un pasaje para viajar a América en el Titanic, el transatlántico más grande y seguro jamás construido.';
@@ -118,9 +117,9 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www111.sbcdnvideo.com/hls/tysxen5gxo66j6cdabwb7wieedbpvrulsbube645qo7gdkh24qzg4p5cvp3q/index-v1-a1.m3u8";
         $peli->save();
-
+ 
         $peli = new Movie();
         $peli->title = "Jurassic World: Mundo Jurásico";
         $peli->description = 'Han pasado 22 años desde que el millonario John Hammond clonara el primer dinosaurio. Con tecnologías mejoradas y nuevas medidas de seguridad, el parque temático "Jurassic World" abre sus puertas.';
@@ -130,21 +129,21 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www289.sbcdnvideo.com/hls/tysxeq2qxo66j6cdabpr7x2qg6hgcnhue5dpl36ayvijstsd335tcwi3uwza/index-v1-a1.m3u8";
         $peli->save();
-
+ 
         $peli = new Movie();
         $peli->title = "Frozen 2";
-        $peli->description = 'Elsa tiene un poder extraordinario: es capaz de crear hielo y nieve. Sin embargo, a pesar de lo feliz que la hacen los habitantes de Arendelle, siente que no encaja allá. Después de oír una voz misteriosa, Elsa, acompañada por Anna, Kristoff, Olaf y Sven, viaja a los bosques embrujados y los mares oscuros que se extienden más allá de los límites de su reino para descubrir quién es en realidad y por qué posee un poder tan asombroso.';
+        $peli->description = 'Elsa tiene un poder extraordinario: es capaz de crear hielo y nieve. Sin embargo, a pesar de lo feliz que la hacen los habitantesde Arendelle, siente que no encaja allá. Después de oír una voz misteriosa, Elsa, acompañada por Anna, Kristoff, Olaf y Sven, viaja a los bosques embrujados y los mares oscuros que se extienden más allá de los límites de su reino para descubrir quién es en realidad y por qué posee un poder tan asombroso.';
         $peli->image = "img/moviesfile/frozen2.jpg";
         $peli->stock = 100;
         $peli->rental_price = 3.99;
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www68.sbcdnvideo.com/hls/tysxe5b5lw66j6cdaa4bvgkzpljyme55shnx3mpsmyfaxjbfdtlgd2ecn4ha/index-v1-a1.m3u8";
         $peli->save();
-
+ 
         $peli = new Movie();
         $peli->title = "Rápidos y furiosos 7";
         $peli->description = 'Hace un año que Dominic y Brian fueron indultados y pudieron regresar a los Estados Unidos. Después de su llegada desean adaptarse a su nueva vida dentro de la legalidad, pero las cosas no son tan fáciles. Dom quiere acercarse a Letty y Brian se acostumbra a la vida en una urbanización con Mia y su hijo.';
@@ -154,9 +153,9 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www107.sbcdnvideo.com/hls/tysxe2g3xo66j6cdablb7hshoy77f24uggz7xme5grrbtsok2tjkdcvgla7a/index-v1-a1.m3u8";
         $peli->save();
-
+ 
         $peli = new Movie();
         $peli->title = "Transformers: La era de la extinción";
         $peli->description = 'Tras la épica batalla que estuvo a punto de acabar con Chicago y con todo el planeta, la Humanidad intenta recuperarse. Sin embargo, otra amenaza, antigua y poderosa, pone a la Tierra en su punto de mira, por lo que los autobots deben actuar.';
@@ -166,9 +165,9 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www93.sbcdnvideo.com/hls/tysxfulzuk66j6cdadwbzxycfeecruiwl22ruxj2gqjlqom6l3gqppgzl2ea/index-v1-a1.m3u8";
         $peli->save();
-
+ 
         $peli = new Movie();
         $peli->title = "Piratas del caribe: la venganza de Salazar";
         $peli->description = 'El capitán Salazar comanda un ejército de piratas fantasma con el que pretende exterminar a todos los piratas vivos de la Tierra, pero Jack Sparrow se opone a él y busca el Tridente de Poseidón, un objeto mágico que permite a quien lo posee controlar los mares, la única esperanza para detener a Salazar.';
@@ -178,9 +177,9 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www233.ff-01.com/token=2jM2oJ6oVediuboib95hEw/1637549701/179.51.0.0/17/d/d6/768e54dd264461e3594121f1e17b9d6d-480p.mp4";
         $peli->save();
-
+ 
         $peli = new Movie();
         $peli->title = "Francotirador";
         $peli->description = 'Chris Kyle, marine del grupo de operaciones especiales del ejército de Estados Unidos, tiene como misión proteger a sus compañeros, acabando con la vida de quien pueda ponerlos en peligro.';
@@ -190,7 +189,67 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www356.ff-01.com/token=66HZpuuVtGDLvua2lJvBqg/1637549852/179.51.0.0/19/9/c6/900400128a60b5a1057450a3e10cdc69-480p.mp4";
+        $peli->save();
+
+ 	    $peli = new Movie();
+        $peli->title = "Kung Fu Panda";
+        $peli->description = 'El panda Po trabaja en la tienda de fideos de su familia y sueña en convertirse en un maestro del kung-fu. Su sueño se hace una realidad cuando es inesperadamente elegido para cumplir una antigua profecía y debe estudiar artes marciales con sus ídolos, los Cinco Furiosos. Po necesitará toda la sabiduría, fortaleza y habilidades para poder proteger a su gente de Tai Lung, un maldito leopardo de nieve.';
+        $peli->image = "img/moviesfile/Kung_Fu_Panda.jpeg";
+        $peli->stock = 100;
+        $peli->rental_price = 3.99;
+        $peli->sale_price = 9.99;
+        $peli->availability = 1;
+        $peli->likes = 0;
+        $peli->url = "https://www883.ff-02.com/token=R6TS8GFYP8Zq-JAct9E_2w/1637549933/179.51.0.0/108/1/5a/a84e22d1ecf30b343b692a5105d945a1-480p.mp4";
+        $peli->save();
+
+	    $peli = new Movie();
+        $peli->title = "Coco";
+        $peli->description = 'Miguel es un niño que sueña con ser músico, pero su familia se lo prohíbe porque su tatarabuelo, músico, los abandonó, y quieren obligar a Miguel a ser zapatero, como todos los miembros de la familia. Por accidente, Miguel entra en la Tierra de los Muertos, de donde sólo podrá salir si un familiar difunto le concede su bendición, pero su tatarabuela se niega a dejarlo volver con los vivos si no promete que no será músico. Debido a eso, Miguel escapa de ella y empieza a buscar a su tatarabuelo.';
+        $peli->image = "img/moviesfile/coco.jpeg";
+        $peli->stock = 100;
+        $peli->rental_price = 3.99;
+        $peli->sale_price = 9.99;
+        $peli->availability = 1;
+        $peli->likes = 0;
+        $peli->url = "https://www883.ff-02.com/token=R6TS8GFYP8Zq-JAct9E_2w/1637549933/179.51.0.0/108/1/5a/a84e22d1ecf30b343b692a5105d945a1-480p.mp4";
+        $peli->save();
+
+	    $peli = new Movie();
+        $peli->title = "Cruella";
+        $peli->description = 'Decidida a convertirse en una exitosa diseñadora de moda, una joven y creativa estafadora llamada Estella se asocia con un par de ladrones para sobrevivir en las calles de Londres.';
+        $peli->image = "img/moviesfile/cruella.jpeg";
+        $peli->stock = 100;
+        $peli->rental_price = 3.99;
+        $peli->sale_price = 9.99;
+        $peli->availability = 1;
+        $peli->likes = 0;
+        $peli->url = "https://www263.sbcdnvideo.com/hls/tysxeytpvc66j6cdaburzvsgpojskk4yagw6lwcbswfpn6qpk6slqbx6ihvq/index-v1-a1.m3u8";
+        $peli->save();
+
+	    $peli = new Movie();
+        $peli->title = "El Justiciero 2";
+        $peli->description = 'El exagente secreto estadounidense Robert McCall continúa combatiendo por su cuenta a secuestradores, maltratadores y otros delincuentes que se cruzan en su solitaria vida de conductor de un servicio de VTC. Ahora, sin embargo, va a tener que intervenir en un asunto que le afecta personalmente.';
+        $peli->image = "img/moviesfile/ElJusticiero2.jpeg";
+        $peli->stock = 100;
+        $peli->rental_price = 3.99;
+        $peli->sale_price = 9.99;
+        $peli->availability = 1;
+        $peli->likes = 0;
+        $peli->url = "https://www271.sbcdnvideo.com/hls/tysxenuhw266j6cdaaar7xaegptlohdn2t3n6rl4c3rutm4mifeteyga62ga/index-v1-a1.m3u8";
+        $peli->save();
+
+	    $peli = new Movie();
+        $peli->title = "Sr. y Sra. Smith ";
+        $peli->description = 'Tras cinco años de idilio conyugal, el matrimonio de John y Jane smith ha entrado en la rutina. Hasta que ambos descubren la verdad: ambos son fríos, letales y carísimos asesinos a sueldo de organizaciones enfrentadas';
+        $peli->image = "img/moviesfile/Sr_&_Sra.Smith.jpg";
+        $peli->stock = 100;
+        $peli->rental_price = 3.99;
+        $peli->sale_price = 9.99;
+        $peli->availability = 1;
+        $peli->likes = 0;
+        $peli->url = "https://www779.ff-02.com/token=0a2fhB7FQuAHPwmjmEVr7Q/1637551632/179.51.0.0/20/5/af/8e7b98b4de42298892b08f21cc864af5-480p.mp4";
         $peli->save();
 
         $peli = new Movie();
@@ -202,7 +261,7 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://s18.upstreamcdn.co/hls/w47rzwtabvbnx2nrozs2ma73k4afygsuhqa6bbunf6sn45lie4t7yvrxll3q/index-v1-a1.m3u8";
         $peli->save();
 
         $peli = new Movie();
@@ -214,7 +273,7 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://s18.upstreamcdn.co/hls/w47rztmaajbnx2nrozxkmxnckq6aydbgfcjka6f7vkvsoijhpo4ytdubptba/index-v1-a1.m3u8";
         $peli->save();
 
         $peli = new Movie();
@@ -226,7 +285,7 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www788.ff-02.com/token=jP30LoOv9pFy8sLMSJv4Hg/1637547364/179.51.0.0/20/b/e4/e18bb648767bb9f928b21fc5a5777e4b-480p.mp4";
         $peli->save();
 
         $peli = new Movie();
@@ -238,7 +297,7 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www7.sbcdnvideo.com/hls/tysxf47ixc66j6cdaczbzrsjf75dqso65hz52p2llkwp33tu5mzte5farxpq/index-v1-a1.m3u8";
         $peli->save();
 
         $peli = new Movie();
@@ -250,7 +309,7 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www872.ff-02.com/token=5xv6nzI9BtxHl0Kys9kO-A/1637547562/179.51.0.0/177/5/6b/cb7fbaf319532e9c6b6504f02607d6b5-480p.mp4";
         $peli->save();
 
         $peli = new Movie();
@@ -262,7 +321,7 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www1831.ff-04.com/token=u2zbP8gCb2zrplQo6FUAIQ/1637547611/179.51.0.0/20/a/ca/baee33de9c6dbab009cd1f8af376fcaa-480p.mp4";
         $peli->save();
 
         $peli = new Movie();
@@ -274,7 +333,7 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://s18.upstreamcdn.co/hls/w47ryxjbanbnx2nrozgkmqx3k77ra3rnn4fq3gbgmszwpsanmf55ca6qer5q/index-v1-a1.m3u8";
         $peli->save();
 
         $peli = new Movie();
@@ -286,7 +345,7 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www1720.ff-04.com/token=ZApOclaM2J9Gz1o3mV4Rpg/1637547909/179.51.0.0/16/f/63/c71ebc54479ee4580440cd2350ecb63f-480p.mp4";
         $peli->save();
 
         $peli = new Movie();
@@ -298,7 +357,7 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www288.sbcdnvideo.com/hls/tysxenhhv666j6cdabjbzgiiezmbrlibwddzem6jp5ehdwpgidxseqdlroka/index-v1-a1.m3u8";
         $peli->save();
 
         $peli = new Movie();
@@ -310,7 +369,7 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www261.sbcdnvideo.com/hls/tysxfdwcro66j6cdadrbvw2tfzhrbgeq35poqlaktgwoxep3s6drv7m4jcea/index-v1-a1.m3u8";
         $peli->save();
 
         $peli = new Movie();
@@ -322,7 +381,7 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www286.sbcdnvideo.com/hls/tysxfxruo266j6cdadbrbhyhoyxt22r6hlfpewkihcu6sekpmtnorufgarsa/index-v1-a1.m3u8";
         $peli->save();
 
         $peli = new Movie();
@@ -334,7 +393,7 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www133.sbcdnvideo.com/hls/tysxfamtwk66j6cdaa2r7tcdfpsgrcdbhvzwfnyjqyvrpgtrkkr2bt26edwa/index-v1-a1.m3u8";
         $peli->save();
 
         $peli = new Movie();
@@ -346,7 +405,7 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www1725.ff-04.com/token=gIMS7xyReiHd1DtvAXr-hQ/1637548230/179.51.0.0/104/f/b0/743143711cbf3fb13753ee28e1672b0f-480p.mp4";
         $peli->save();
 
         $peli = new Movie();
@@ -358,7 +417,7 @@ class UsersTableSeeder extends Seeder
         $peli->sale_price = 9.99;
         $peli->availability = 1;
         $peli->likes = 0;
-        $peli->url = "";
+        $peli->url = "https://www16.ff-01.com/token=1ilSrW9O4-qjQlndBEJhSg/1637548278/179.51.0.0/19/6/e2/358556ce67d8bc8e2aa367b924b22e26-480p.mp4";
         $peli->save();
 
         // -------------------------- likes ---------------------------------------------
